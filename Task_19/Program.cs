907a9493cb05c0d935ecb0ba882e1ff747e21ce6
Diana <diana@example.com>
2022-07-02 02:32:55 +0300

    Добавлено решение через инверсию

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -10,8 +10,8 @@ int num = Convert.ToInt32(Console.ReadLine());
 List<int> ListNum(int n)
 {
     List<int> number = new List<int>();
-    number.Insert(0, n % 10);
-    n = n / 10;
+    number.Insert(0, n % 10); // Эти две строчки на случай, если введеное число 0, 
+    n = n / 10;               // и чтобы метод записал его в list
     while (n != 0)
     {
         number.Insert(0, n % 10);
@@ -33,4 +33,20 @@ for (int i = 0; i < numList.Count / 2; i++)
 }
 
 if (paper == 0) Console.WriteLine("Да, число является полиндромом.");
+else Console.WriteLine("Нет, число не является полиндромом.");
+
+// Через инверсию числа:
+int InversionNum(int n)
+{
+    int invNum = 0;
+    while (n != 0)
+    {
+        invNum = invNum * 10 + n % 10;
+        n = n / 10;
+    }
+    return invNum;
+}
+
+Console.WriteLine($"Инверсия введенного числа {InversionNum(num)}");
+if (num == InversionNum(num)) Console.WriteLine("Да, число является полиндромом.");
 else Console.WriteLine("Нет, число не является полиндромом.");
