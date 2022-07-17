76bcbe3ef954ba50e81f01792b8f92e41fdefe63
Diana <diana@example.com>
2022-07-17 20:11:18 +0300

    Задание 52

---
+++ Task_52/Program.cs
@@ -0,0 +1,67 @@
+﻿/* Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. */
+
+int[,] CreateMatrixRndInt(int m, int n, int min, int max)
+{
+    int[,] matr = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write($"[{matr[i, j],3} |");
+            else if (j < matr.GetLength(1) - 1) Console.Write($"{matr[i, j],3} |");
+            else Console.WriteLine($"{matr[i, j],3} ]");
+        }
+    }
+}
+
+double[] AverageByColumn(int[,] matr)
+{
+    double[] arr = new double[matr.GetLength(1)];
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            sum = sum + matr[i, j];
+        }
+        arr[j] = Math.Round(sum / matr.GetLength(0), 2);
+    }
+    return arr;
+}
+
+void PrintArray(double[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0) Console.Write($"[{arr[i]} , ");
+        else if (i < arr.Length - 1) Console.Write($"{arr[i]} , ");
+        else Console.WriteLine($"{arr[i]}]");
+    }
+}
+
+int[,] matrix = CreateMatrixRndInt(3, 2, 0, 10);
+PrintMatrix(matrix);
+double[] average = AverageByColumn(matrix);
+PrintArray(average);
