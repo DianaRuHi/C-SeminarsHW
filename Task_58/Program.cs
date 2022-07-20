85f95b68bff18e2dc552f35614cefec08a9bbc34
Diana <diana@example.com>
2022-07-21 01:03:35 +0300

    Задача 58

---
+++ Task_58/Program.cs
@@ -0,0 +1,61 @@
+﻿/* Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18 */
+
+int[,] CreateMatrixRndInt(int m, int n, int min, int max)
+{
+    int[,] matr = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write($"[{matr[i, j],3} |");
+            else if (j < matr.GetLength(1) - 1) Console.Write($"{matr[i, j],3} |");
+            else Console.WriteLine($"{matr[i, j],3} ]");
+        }
+    }
+}
+
+int[,] MatrixProduct(int[,] matr1, int[,] matr2)
+{
+    int[,] result = new int[matr1.GetLength(0), matr2.GetLength(1)];
+    for (int i = 0; i < matr1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr2.GetLength(1); j++)
+        {
+            for (int count = 0; count < matr1.GetLength(1); count++)
+            {
+                result[i, j] = result[i, j] + matr1[i, count] * matr2[count, j];
+            }
+        }
+    }
+    return result;
+}
+
+int[,] matrix1 = CreateMatrixRndInt(4, 2, 0, 5);
+Console.WriteLine("Первая матрица:");
+PrintMatrix(matrix1);
+int[,] matrix2 = CreateMatrixRndInt(2, 3, 0, 5);
+Console.WriteLine("Вторая матрица:");
+PrintMatrix(matrix2);
+int[,] product = MatrixProduct(matrix1, matrix2);
+Console.WriteLine("Произведение матриц:");
+PrintMatrix(product);
