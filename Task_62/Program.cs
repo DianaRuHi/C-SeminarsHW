3a129a7d8b40ebb920f5ac9fd496b2b171df1781
Diana <diana@example.com>
2022-07-21 18:17:33 +0300

    Задача 62 hallelujah

--- Task_62/Program.cs
+++ Task_62/Program.cs
@@ -5,7 +5,7 @@
 11 16 15 06
 10 09 08 07 */
 
-void FillRowOrColumn(int[,] matr, int iStart, int jStart, int iEnd, int jEnd, int number)
+int FillRowOrColumn(int[,] matr, int iStart, int jStart, int iEnd, int jEnd, int number)
 {
     if (iStart <= iEnd && jStart <= jEnd)
     {
@@ -29,25 +29,26 @@ void FillRowOrColumn(int[,] matr, int iStart, int jStart, int iEnd, int jEnd, in
             }
         }
     }
+    return number;
 }
 
-void FillPerimeter(int[,] matr, int iStartDiagnal, int jStartDiagnal, int iEndDiagnal, int jEndDiagnal, int number)
+int FillPerimeter(int[,] matr, int iStartDiagnal, int jStartDiagnal, int iEndDiagnal, int jEndDiagnal, int number)
 {
-    if ((iStartDiagnal - iEndDiagnal) == 0 || (jStartDiagnal - jEndDiagnal) == 0)
-        FillRowOrColumn(matr, iStartDiagnal, jStartDiagnal, iStartDiagnal, jEndDiagnal, number);
-    else if ((iStartDiagnal - iEndDiagnal) == 1)
+    if ((iEndDiagnal - iStartDiagnal) == 0 || (jEndDiagnal - jStartDiagnal) == 0)
+        number = FillRowOrColumn(matr, iStartDiagnal, jStartDiagnal, iEndDiagnal, jEndDiagnal, number);
+    else if ((iEndDiagnal - iStartDiagnal) == 1)
     {
-        FillRowOrColumn(matr, iStartDiagnal, jStartDiagnal, iStartDiagnal, jEndDiagnal, number);
-        FillRowOrColumn(matr, iStartDiagnal + 1, jEndDiagnal, iEndDiagnal, jEndDiagnal, number);
-        FillRowOrColumn(matr, iEndDiagnal, jEndDiagnal - 1, iEndDiagnal, jStartDiagnal, number);
+        number = FillRowOrColumn(matr, iStartDiagnal, jStartDiagnal, iStartDiagnal, jEndDiagnal, number);
+        number = FillRowOrColumn(matr, iEndDiagnal, jEndDiagnal, iEndDiagnal, jStartDiagnal, number);
     }
     else
     {
-        FillRowOrColumn(matr, iStartDiagnal, jStartDiagnal, iStartDiagnal, jEndDiagnal, number);
-        FillRowOrColumn(matr, iStartDiagnal + 1, jEndDiagnal, iEndDiagnal, jEndDiagnal, number);
-        FillRowOrColumn(matr, iEndDiagnal, jEndDiagnal - 1, iEndDiagnal, jStartDiagnal, number);
-        FillRowOrColumn(matr, iEndDiagnal - 1, jStartDiagnal, iStartDiagnal + 1, jStartDiagnal, number);
+        number = FillRowOrColumn(matr, iStartDiagnal, jStartDiagnal, iStartDiagnal, jEndDiagnal, number);
+        number = FillRowOrColumn(matr, iStartDiagnal + 1, jEndDiagnal, iEndDiagnal, jEndDiagnal, number);
+        number = FillRowOrColumn(matr, iEndDiagnal, jEndDiagnal - 1, iEndDiagnal, jStartDiagnal, number);
+        number = FillRowOrColumn(matr, iEndDiagnal - 1, jStartDiagnal, iStartDiagnal + 1, jStartDiagnal, number);
     }
+    return number;
 }
 
 int[,] FillArray(int row, int collumn)
@@ -59,7 +60,7 @@ int[,] FillArray(int row, int collumn)
     int count = (row < collumn) ? (row / 2 + row % 2) : (collumn / 2 + collumn % 2);
     for (int i = 0; i < count; i++)
     {
-        FillPerimeter(result, iStartD + i, jStartD + i, iEndD - i, jEndD - i, number);
+        number = FillPerimeter(result, iStartD + i, jStartD + i, iEndD - i, jEndD - i, number);
     }
     return result;
 }
@@ -77,12 +78,13 @@ void PrintMatrix(int[,] matr)
     }
 }
 
-int[,] matrixMagic = FillArray(5, 7);
+int[,] matrixMagic = FillArray(6, 9);
 PrintMatrix(matrixMagic);
 
-// Вывод: 
-// [  1 |  2 |  3 |  4 |  5 |  6 |  7 ]
-// [  3 |  1 |  2 |  3 |  4 |  5 |  1 ]
-// [  2 |  1 |  1 |  2 |  3 |  1 |  2 ]
-// [  1 |  4 |  3 |  2 |  1 |  2 |  3 ]
-// [  6 |  5 |  4 |  3 |  2 |  1 |  4 ]
+//Вывод
+//[  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 ]
+//[ 26 | 27 | 28 | 29 | 30 | 31 | 32 | 33 | 10 ]
+//[ 25 | 44 | 45 | 46 | 47 | 48 | 49 | 34 | 11 ]
+//[ 24 | 43 | 54 | 53 | 52 | 51 | 50 | 35 | 12 ]
+//[ 23 | 42 | 41 | 40 | 39 | 38 | 37 | 36 | 13 ]
+//[ 22 | 21 | 20 | 19 | 18 | 17 | 16 | 15 | 14 ]
